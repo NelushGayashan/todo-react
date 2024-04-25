f5b7ee0d6263f88d7473e107ea715ab156f52ee4
NGFXavier <nelushgayashan@gmail.com>
2024-04-25 19:30:39 +0530

    After adding the pop-up messages

--- src/App.js
+++ src/App.js
@@ -1,157 +1,228 @@
-// App.js File 
-import React, { Component } from "react"; 
-import "bootstrap/dist/css/bootstrap.css"; 
-import Container from "react-bootstrap/Container"; 
-import Row from "react-bootstrap/Row"; 
-import Col from "react-bootstrap/Col"; 
-import Button from "react-bootstrap/Button"; 
-import InputGroup from "react-bootstrap/InputGroup"; 
-import FormControl from "react-bootstrap/FormControl"; 
-import ListGroup from "react-bootstrap/ListGroup"; 
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+import InputGroup from "react-bootstrap/InputGroup";
+import FormControl from "react-bootstrap/FormControl";
+import ListGroup from "react-bootstrap/ListGroup";
+import Modal from "react-bootstrap/Modal";
 
-class App extends Component { 
-	constructor(props) { 
-		super(props); 
 
-		// Setting up state 
-		this.state = { 
-			userInput: "", 
-			list: [], 
-		}; 
-	} 
+class App extends Component {
+    constructor(props) {
+        super(props);
 
-	// Set a user input value 
-	updateInput(value) { 
-		this.setState({ 
-			userInput: value, 
-		}); 
-	} 
+        this.state = {
+            userInput: "",
+            list: [],
+            showModal: false,
+            editIndex: -1
+        };
+    }
 
-	// Add item if user input in not empty 
-	addItem() { 
-		if (this.state.userInput !== "") { 
-			const userInput = { 
-				// Add a random id which is used to delete 
-				id: Math.random(), 
+    updateInput(value) {
+        this.setState({
+            userInput: value
+        });
+    }
 
-				// Add a user value to list 
-				value: this.state.userInput, 
-			}; 
+    addItem() {
+        if (this.state.userInput.trim() !== "") {
+            const userInput = {
+                id: Math.random(),
+                value: this.state.userInput
+            };
+            const list = [...this.state.list];
+            list.push(userInput);
+            this.setState({
+                list,
+                userInput: ""
+            });
+        } else {
+            toast.error("Please enter a task before adding.", {
+                position: "top-right",
+                autoClose: 3000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+            });
+        }
+    }
 
-			// Update list 
-			const list = [...this.state.list]; 
-			list.push(userInput); 
 
-			// reset state 
-			this.setState({ 
-				list, 
-				userInput: "", 
-			}); 
-		} 
-	} 
+    deleteItem(key) {
+        const list = [...this.state.list];
+        const deletedItem = list.find((item) => item.id === key);
+        const updateList = list.filter((item) => item.id !== key);
+        this.setState({
+            list: updateList
+        });
+        toast.success(`Deleted task: ${deletedItem.value}`, {
+            position: "top-right",
+            autoClose: 3000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+        });
+    }
 
-	// Function to delete item from list use id to delete 
-	deleteItem(key) { 
-		const list = [...this.state.list]; 
 
-		// Filter values and leave value which we need to delete 
-		const updateList = list.filter((item) => item.id !== key); 
 
-		// Update list in state 
-		this.setState({ 
-			list: updateList, 
-		}); 
-	} 
+    handleShowModal = (index) => {
+        this.setState({
+            showModal: true,
+            editIndex: index,
+            userInput: this.state.list[index].value
+        });
+    };
 
-	editItem = (index) => { 
-	const todos = [...this.state.list]; 
-	const editedTodo = prompt('Edit the todo:'); 
-	if (editedTodo !== null && editedTodo.trim() !== '') { 
-		let updatedTodos = [...todos] 
-		updatedTodos[index].value= editedTodo 
-		this.setState({ 
-		list: updatedTodos, 
-	}); 
-	} 
-	} 
+    handleCloseModal = () => {
+        this.setState({
+            showModal: false,
+            editIndex: -1,
+            userInput: ""
+        });
+    };
 
-	render() { 
-		return ( 
-			<Container> 
-				<Row 
-					style={{ 
-						display: "flex", 
-						justifyContent: "center", 
-						alignItems: "center", 
-						fontSize: "3rem", 
-						fontWeight: "bolder", 
-					}} 
-				> 
-					TODO LIST 
-				</Row> 
+    handleEditItem = () => {
+        const { editIndex, userInput, list } = this.state;
+        if (userInput.trim() !== "") {
+            const updatedList = [...list];
+            const editedTask = updatedList[editIndex].value; // Get the task name before updating it
+            updatedList[editIndex].value = userInput;
+            this.setState({
+                list: updatedList,
+                userInput: "",
+                showModal: false,
+                editIndex: -1
+            });
+            toast.success(`Edited task: ${editedTask} successfully`, {
+                position: "top-right",
+                autoClose: 3000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+            });
+        }
+    };
 
-				<hr /> 
-				<Row> 
-					<Col md={{ span: 5, offset: 4 }}> 
-						<InputGroup className="mb-3"> 
-							<FormControl 
-								placeholder="add item . . . "
-								size="lg"
-								value={this.state.userInput} 
-								onChange={(item) => 
-									this.updateInput(item.target.value) 
-								} 
-								aria-label="add something"
-								aria-describedby="basic-addon2"
-							/> 
-							<InputGroup> 
-								<Button 
-									variant="dark"
-									className="mt-2"
-									onClick={() => this.addItem()} 
-								> 
-									ADD 
-								</Button> 
-							</InputGroup> 
-						</InputGroup> 
-					</Col> 
-				</Row> 
-				<Row> 
-					<Col md={{ span: 5, offset: 4 }}> 
-						<ListGroup> 
-							{/* map over and print items */} 
-							{this.state.list.map((item, index) => { 
-								return ( 
-								<div key = {index} > 
-									<ListGroup.Item 
-										variant="dark"
-										action 
-										style={{display:"flex", 
-												justifyContent:'space-between'
-									}} 
-									> 
-										{item.value} 
-										<span> 
-										<Button style={{marginRight:"10px"}} 
-										variant = "light"
-										onClick={() => this.deleteItem(item.id)}> 
-										Delete 
-										</Button> 
-										<Button variant = "light"
-										onClick={() => this.editItem(index)}> 
-										Edit 
-										</Button> 
-										</span> 
-									</ListGroup.Item> 
-								</div> 
-								); 
-							})} 
-						</ListGroup> 
-					</Col> 
-				</Row> 
-			</Container> 
-		); 
-	} 
-} 
 
-export default App; 
+    render() {
+        return (
+            <Container>
+                <ToastContainer />
+                <Row
+                    style={{
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                        fontSize: "3rem",
+                        fontWeight: "bolder"
+                    }}
+                >
+                    TODO LIST
+                </Row>
+                <hr />
+                <Row>
+                    <Col md={{ span: 5, offset: 4 }}>
+                        <InputGroup className="mb-3">
+                            <FormControl
+                                placeholder="add item . . . "
+                                size="lg"
+                                value={this.state.userInput}
+                                onChange={(item) =>
+                                    this.updateInput(item.target.value)
+                                }
+                                aria-label="add something"
+                                aria-describedby="basic-addon2"
+                            />
+                            <InputGroup>
+                                <Button
+                                    variant="dark"
+                                    className="mt-2"
+                                    onClick={() => this.addItem()}
+                                >
+                                    ADD
+                                </Button>
+                            </InputGroup>
+                        </InputGroup>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col md={{ span: 5, offset: 4 }}>
+                        <ListGroup>
+                            {this.state.list.map((item, index) => {
+                                return (
+                                    <div key={index}>
+                                        <ListGroup.Item
+                                            variant="dark"
+                                            action
+                                            style={{
+                                                display: "flex",
+                                                justifyContent: "space-between"
+                                            }}
+                                        >
+                                            {item.value}
+                                            <span>
+                                                <Button
+                                                    style={{ marginRight: "10px" }}
+                                                    variant="light"
+                                                    onClick={() =>
+                                                        this.deleteItem(item.id)
+                                                    }
+                                                >
+                                                    Delete
+                                                </Button>
+                                                <Button
+                                                    variant="light"
+                                                    onClick={() =>
+                                                        this.handleShowModal(index)
+                                                    }
+                                                >
+                                                    Edit
+                                                </Button>
+                                            </span>
+                                        </ListGroup.Item>
+                                    </div>
+                                );
+                            })}
+                        </ListGroup>
+                    </Col>
+                </Row>
+                <Modal show={this.state.showModal} onHide={this.handleCloseModal}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Edit Item</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <FormControl
+                            placeholder="Edit item..."
+                            value={this.state.userInput}
+                            onChange={(e) => this.updateInput(e.target.value)}
+                        />
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={this.handleCloseModal}>
+                            Close
+                        </Button>
+                        <Button variant="primary" onClick={this.handleEditItem}>
+                            Save Changes
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
+            </Container>
+        );
+    }
+
+}
+
+export default App;
